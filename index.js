6096c3fee233255fa9fa46f2d868234bc291702a
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-10 00:14:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on('message', msg => {
         .setColor('BLUE');
     
     const embedPages = [embed1, embed2, embed3];
-    ButtonPages.createPages(client.interaction, msg, embedPages, 60 * 1000, "blurple", "▶ ", "◀", "❌");
+    ButtonPages.createPages(client.interaction, msg, embedPages, 60 * 1000, "blurple", ":smile:", ":smile:", ":smile:");
   }
     
 });
