e5b338c825000178905ba5188a70994764036186
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-09 23:12:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log("Ready")
 
 client.on('message', msg => {
     if(message.content === "!ping")
-        return message.channel.send("Pong " + client.ws.ping);
+        return message.channel.send("Pong ");
 });
 
 client.login("OTg0NTcwODI2MjA2NjIxNzI3.GqSxX1.tyc-Gp6zXIulYdWlGFfo6W2e05WJM2Jcq6wn_8");
