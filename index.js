2b04935d1e6bbf9cd9ffc52cac8040f0910da454
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-10 20:16:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,13 +25,18 @@ client.on('message', msg => {
     //        msg.channel.send("Kiralvfalva, Gyoma, Méhkerék, Hadur Várus, Óvárus, Horka, Hévíz, Feheloval, Rozsdáskaszát, Hosvarosa, Kisbér, Tura, Hatvan", {files: ["https://static.wixstatic.com/media/ef5476_4d570f16907047df8d6c7bebd2c176e9~mv2.png/v1/fill/w_850,h_844,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20City%201.png"]});
      //       msg.channel.send("Vaja, Rátót Var", {files: ["https://static.wixstatic.com/media/ef5476_b14d489b0ef24761b19ebeb375ba7dc8~mv2.png/v1/fill/w_870,h_864,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20Valley%20Fortress.png"]});
       //      msg.channel.send("Ruda, Sovica, Csorna, Délrétek, Tavasz, Bejárat, Kistemplom, Vaseke", {files: ["https://static.wixstatic.com/media/ef5476_faf687624cb14fcfbb07efccf422a133~mv2.png/v1/fill/w_829,h_824,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20Village%203.png"]});
-
+	//  if(TWCity1.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(msg.content.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")))
+     //   msg.channel.send(msg.content.normalize("NFD").replace(/[\u0300-\u036f]/g, ""), {files: ["https://static.wixstatic.com/media/ef5476_4d570f16907047df8d6c7bebd2c176e9~mv2.png/v1/fill/w_850,h_844,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20City%201.png"]});
+	//	
    
 
-    if(msg.content.includes("!")){
-	//	if(TWCity1.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(msg.content.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))){
-     //   msg.channel.send(msg.content.normalize("NFD").replace(/[\u0300-\u036f]/g, ""), {files: ["https://static.wixstatic.com/media/ef5476_4d570f16907047df8d6c7bebd2c176e9~mv2.png/v1/fill/w_850,h_844,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20City%201.png"]});
-	//	}
+    if(msg.content === "!maps"){
+    const embed1 = new Discord.MessageEmbed()
+        .setTitle('Ungverija')
+        .setImage('https://static.wixstatic.com/media/ef5476_4d570f16907047df8d6c7bebd2c176e9~mv2.png/v1/fill/w_850,h_844,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20City%201.png')
+        .setDescription('Kiralvfalva, Gyoma, Méhkerék, Hadur Várus, Óvárus, Horka, Hévíz, Feheloval, Rozsdáskaszát, Hosvarosa, Kisbér, Tura, Hatvan')
+        .setColor('BLUE');
+
     const embed2 = new Discord.MessageEmbed()
        .setTitle('Ungverija')
        .setImage('https://static.wixstatic.com/media/ef5476_b14d489b0ef24761b19ebeb375ba7dc8~mv2.png/v1/fill/w_870,h_864,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20Valley%20Fortress.png')
