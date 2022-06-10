31205254b379ea4335d3286212f2465f8c482bda
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-10 19:22:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ process.on("unhandledRejection", (reason, promise) => {
 	//client.channels.cache.get(error_logs).send({ embeds: [rejectionembed] })
 });
   
-client.login(botToken).then(() => {
+client.login(process.env.TOKEN).then(() => {
 	console.log(
 	  chalk.bgBlueBright.black(
 		` Successfully logged in as: ${client.user.username}#${client.user.discriminator} `
