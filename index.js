24d320001d9326fb98220a45168ccbdb12bcc6d7
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-10 19:35:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ client.on('clickButton', (button) => {
   ButtonPages.buttonInteractions(button, client.interaction);
 });
 
+const TWCity1 = ["Kiralvfalva", "Gyoma", "Méhkerék", "Hadur Várus", "Óvárus", "Horka", "Hévíz", "Feheloval", "Rozsdáskaszát", "Hosvarosa, Kisbér", "Tura", "Hatvan"]
 
 client.on('message', msg => {
   
@@ -27,13 +28,10 @@ client.on('message', msg => {
 
    
 
-    if(msg.content === "!maps"){
-    const embed1 = new Discord.MessageEmbed()
-        .setTitle('Ungverija')
-        .setImage('https://static.wixstatic.com/media/ef5476_4d570f16907047df8d6c7bebd2c176e9~mv2.png/v1/fill/w_850,h_844,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20City%201.png')
-        .setDescription('Kiralvfalva, Gyoma, Méhkerék, Hadur Várus, Óvárus, Horka, Hévíz, Feheloval, Rozsdáskaszát, Hosvarosa, Kisbér, Tura, Hatvan')
-        .setColor('BLUE');
-
+    if(msg.content.includes("!")){
+		if(TWCity1.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(msg.content.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))){
+        msg.channel.send(msg.content.normalize("NFD").replace(/[\u0300-\u036f]/g, ""), {files: ["https://static.wixstatic.com/media/ef5476_4d570f16907047df8d6c7bebd2c176e9~mv2.png/v1/fill/w_850,h_844,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20City%201.png"]});
+		}
     const embed2 = new Discord.MessageEmbed()
        .setTitle('Ungverija')
        .setImage('https://static.wixstatic.com/media/ef5476_b14d489b0ef24761b19ebeb375ba7dc8~mv2.png/v1/fill/w_870,h_864,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20Valley%20Fortress.png')
