782371ae88912731ab87e5e672691ae711de7908
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-10 14:49:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Commands = [];
 const cmdFiles = readdirSync("./commands").filter((file) =>
 	file.endsWith(".js"),
 );
-
+ 
 Bot.on("ready", async () => {
 	for (const fileName of cmdFiles) {
 		const File = require(`./commands/${fileName}`);
