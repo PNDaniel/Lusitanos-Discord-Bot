c2d168fefb34668d24c3b53f62562a807ea192f4
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-11 22:55:11 +0100

    .

--- index.js
+++ index.js
@@ -100,7 +100,11 @@ client.on('message', async msg => {
 		try {
 			if (fs.existsSync(`${path}/${matches['bestMatch']['target']}_img.png`) && fs.existsSync(`${path}/${matches['bestMatch']['target']}_vet.png`) ) {
 				msg.delete();
+				if(matches['bestMatch']['target']=='fortebraccio')
+				msg.channel.send({files: [`${path}/${matches['bestMatch']['target']}_img.png`,`${path}/${matches['bestMatch']['target']}_vet.png`,`${path}/${matches['bestMatch']['target']}_doc.png`]});
+				else
 				msg.channel.send({files: [`${path}/${matches['bestMatch']['target']}_img.png`,`${path}/${matches['bestMatch']['target']}_vet.png`]});
+
 			}else{
 				msg.channel.send("Não existe uma unidade com esse nome");
 			}
