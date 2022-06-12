c209fcec942cad7b37b32d4b16e72d0d2ca5aeae
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-12 23:55:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,33 @@ client.on('ready', () => {
 
 
 
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}]; 
+
+const rest = new REST({ version: '9' }).setToken('token');
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+
+
 // private message:  msg.author.send("Your message here.")
 
 const TWCity1 = ["marraquexe", "kiralvfalva", "gyoma", "mehkerek", "hadur varus", "ovarus", "horka", "heviz", "feheloval", "rozsdaskaszat", "hosvarosa", " kisber", "tura", "hatvan"]
