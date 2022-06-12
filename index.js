96fc526b16e9dcbb8c863abbb4d8595d001175f6
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-13 00:03:59 +0100

    .

--- index.js
+++ index.js
@@ -19,32 +19,6 @@ client.on('ready', () => {
 
 
 
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types');
-
-const commands = [{
-  name: 'ping',
-  description: 'Replies with Pong!'
-}]; 
-
-const rest = new REST({ version: '9' }).setToken('token');
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: commands },
-    );
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
-
 
 // private message:  msg.author.send("Your message here.")
 
