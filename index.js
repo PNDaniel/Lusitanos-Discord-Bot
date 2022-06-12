87a87e391b031d88431a4a6941e97793f9592136
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-13 00:07:41 +0100

    .

--- index.js
+++ index.js
@@ -17,7 +17,19 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
 
 
 // private message:  msg.author.send("Your message here.")
