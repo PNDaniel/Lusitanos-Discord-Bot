660d175e2a5889ab0d6895904a6a732008e7e360
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-12 20:13:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -139,10 +139,22 @@ client.on('message', async msg => {
 
 					var data = await get_unit_linkV3(matches['bestMatch']['target']);
 
+					
 
+					const Embed = new Discord.MessageEmbed()
+									.setColor('#0099ff')
+									.setTitle('Guide for '+"**" + matches['bestMatch']['target'].charAt(0).toUpperCase() + matches['bestMatch']['target'].slice(1).replace("_", " ") + "**")
+									.setDescription('Unit guide text can be implemented here!')
+									//.setThumbnail(data[0])
+									//.setImage(data[1])
+					const Embed1 = new Discord.MessageEmbed()
+									.setColor('#0099ff')
+									.setDescription('Veterancy')
+									//.setThumbnail(data[0])
+									//.setImage(data[2])
 
-					const embed1 = new Discord.MessageEmbed().setTitle('unit').setImage(data[0]);
-					msg.channel.send(embed1)
+					msg.channel.send({ embeds: [Embed, Embed1] });
+
 			
 
 				} else {
