5e300d33caf154015fe9b72af3debaa7d1240458
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-12 21:08:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -238,8 +238,6 @@ const  [link_img, link_vet, link_doc] =  await Promise.all([
 		axios.get(`https://drive.google.com/uc?id=${id_img}`),
 		axios.get(`https://drive.google.com/uc?id=${id_vet}`)
 	  ])
-	  msg.channel.send(3)			
-
 	  return [link_img.request.res.req._redirectable._currentUrl,link_vet.request.res.req._redirectable._currentUrl]
 
 }
