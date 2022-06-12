2261f60f125b23683e56980c05f1322f6359d027
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-12 01:34:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,167 +16,176 @@ const purge_messages = 1000 * 60 * 10; //10 minutes
 const ID_channel_to_delete = '984569178293940297';
 const ID_channel_request = '985278059001290753';
 
- 
-client.on('ready' ,() => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('clickButton', (button) => {
-  ButtonPages.buttonInteractions(button, client.interaction);
+	ButtonPages.buttonInteractions(button, client.interaction);
 });
 // private message:  msg.author.send("Your message here.")
 
-const TWCity1 = ["marraquexe","kiralvfalva", "gyoma", "mehkerek", "hadur varus", "ovarus", "horka", "heviz", "feheloval", "rozsdaskaszat", "hosvarosa"," kisber", "tura", "hatvan"]
+const TWCity1 = ["marraquexe", "kiralvfalva", "gyoma", "mehkerek", "hadur varus", "ovarus", "horka", "heviz", "feheloval", "rozsdaskaszat", "hosvarosa", " kisber", "tura", "hatvan"]
 const TWValleyFortress = ['vaja', 'ratot var']
-const TWVillage3 = ['ruda', 'sovica', 'csorna',' delretek', 'tavasz', 'bejarat', 'kistemplom', 'vaseke']
+const TWVillage3 = ['ruda', 'sovica', 'csorna', ' delretek', 'tavasz', 'bejarat', 'kistemplom', 'vaseke']
 
 client.on('message', async msg => {
 
-	if(TWCity1.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/", "")) && msg.content.includes("/")){
-	   msg.delete();
-	   if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
-	   msg.channel.send({files: ["https://static.wixstatic.com/media/ef5476_4d570f16907047df8d6c7bebd2c176e9~mv2.png/v1/fill/w_850,h_844,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20City%201.png"]});
-	}
-   	
-	if(TWValleyFortress.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/", "")) && msg.content.includes("/")){
-	   msg.delete();
-	   if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
-	   msg.channel.send({files: ["https://static.wixstatic.com/media/ef5476_b14d489b0ef24761b19ebeb375ba7dc8~mv2.png/v1/fill/w_870,h_864,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20Valley%20Fortress.png"]});
-	}
-
-	if(TWVillage3.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/", "")) && msg.content.includes("/")){
-	   msg.delete();
-	   if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
-	   msg.channel.send({files: ["https://static.wixstatic.com/media/ef5476_faf687624cb14fcfbb07efccf422a133~mv2.png/v1/fill/w_829,h_824,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20Village%203.png"]});
-	}
-
-	if(TWCity1.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/equal=", "")) && msg.content.includes("/equal=")){
-	   msg.delete();
-	   if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
-	   msg.channel.send("Kiralvfalva\n Gyoma\n Méhkerék\n Hadur Várus\n Óvárus\n Horka\n Hévíz \nFeheloval\n Rozsdáskaszát\n Hosvarosa\n Kisbér\n Tura\n Hatvan")
-	}
-
-	if(TWValleyFortress.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/equal=", "")) && msg.content.includes("/equal=")){
+	if (TWCity1.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/", "")) && msg.content.includes("/")) {
 		msg.delete();
 		if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
-		msg.channel.send("Vaja \n Ratót Var")
-	 }
+			msg.channel.send({
+				files: ["https://static.wixstatic.com/media/ef5476_4d570f16907047df8d6c7bebd2c176e9~mv2.png/v1/fill/w_850,h_844,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20City%201.png"]
+			});
+	}
 
-	 if(TWVillage3.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/equal=", "")) && msg.content.includes("/equal=")){
+	if (TWValleyFortress.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/", "")) && msg.content.includes("/")) {
 		msg.delete();
 		if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
-		msg.channel.send("Ruda\nSovica\nCsorna\nDélrétek\nTavasz\nBejárat\nKistemplom\nVaseke") 
-	 }
-	 if(msg.content.includes("/player=")) {
+			msg.channel.send({
+				files: ["https://static.wixstatic.com/media/ef5476_b14d489b0ef24761b19ebeb375ba7dc8~mv2.png/v1/fill/w_870,h_864,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20Valley%20Fortress.png"]
+			});
+	}
+
+	if (TWVillage3.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/", "")) && msg.content.includes("/")) {
+		msg.delete();
+		if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
+			msg.channel.send({
+				files: ["https://static.wixstatic.com/media/ef5476_faf687624cb14fcfbb07efccf422a133~mv2.png/v1/fill/w_829,h_824,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/TW%20Village%203.png"]
+			});
+	}
+
+	if (TWCity1.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/equal=", "")) && msg.content.includes("/equal=")) {
+		msg.delete();
+		if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
+			msg.channel.send("Kiralvfalva\n Gyoma\n Méhkerék\n Hadur Várus\n Óvárus\n Horka\n Hévíz \nFeheloval\n Rozsdáskaszát\n Hosvarosa\n Kisbér\n Tura\n Hatvan")
+	}
+
+	if (TWValleyFortress.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/equal=", "")) && msg.content.includes("/equal=")) {
+		msg.delete();
+		if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
+			msg.channel.send("Vaja \n Ratót Var")
+	}
+
+	if (TWVillage3.includes(msg.content.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace("/equal=", "")) && msg.content.includes("/equal=")) {
+		msg.delete();
+		if (msg.member.roles.cache.some(role => role.name === 'Conselho'))
+			msg.channel.send("Ruda\nSovica\nCsorna\nDélrétek\nTavasz\nBejárat\nKistemplom\nVaseke")
+	}
+	if (msg.content.includes("/player=")) {
 		msg.delete();
 		const [a] = await Promise.all([get_google_sheets(msg.content.replace("/player=", ""))]);
-		msg.channel.send(a) 
-	 }
+		msg.channel.send(a)
+	}
 
-	 if(msg.content.includes("/request=")){
+	if (msg.content.includes("/request=")) {
 		msg.attachments.forEach(attachment => {
 			const ImageLink = attachment.proxyURL;
 			msg.delete();
 			msg.author.send("Update has been requested!")
-			msg.author.send({files: [ImageLink]})
-			client.channels.cache.get(ID_channel_request).send(msg.author.username + " :\n\n\n" +msg.content.replace("/request=",""));
-			client.channels.cache.get(ID_channel_request).send({files: [ImageLink]});
+			msg.author.send({
+				files: [ImageLink]
+			})
+			client.channels.cache.get(ID_channel_request).send(msg.author.username + " :\n\n\n" + msg.content.replace("/request=", ""));
+			client.channels.cache.get(ID_channel_request).send({
+				files: [ImageLink]
+			});
 		});
-	 }
+	}
 
 
 
-	 if(msg.content.includes("/tropa=") || msg.content.includes("/tropas=")){
-		var unit = msg.content.toLowerCase().replace("/tropa=","").replace(" ","_").replace("/tropas=","")
+	if (msg.content.includes("/tropa=") || msg.content.includes("/tropas=")) {
+		var unit = msg.content.toLowerCase().replace("/tropa=", "").replace(" ", "_").replace("/tropas=", "")
 		var files = fs.readdirSync(path)
-		var files_that_exist= [];
+		var files_that_exist = [];
 
 		for (const file of files) {
-			files_that_exist.push(file.replace("_vet.png","").replace("_img.png","").replace("_doc.png",""))
-			
-		  }
-		while(unit.charAt(0) === '_')
-			{
-				unit = unit.substring(1);
-			}
-			var matches = stringSimilarity.findBestMatch(unit, files_that_exist);
-		  try {
-			if (fs.existsSync(`${path}/${matches['bestMatch']['target']}_img.png`) && fs.existsSync(`${path}/${matches['bestMatch']['target']}_vet.png`) ) {
-			if(fs.existsSync(`${path}/${matches['bestMatch']['target']}_doc.png`)){	
-				delete_all_expect_pin()
-				msg.delete();
-				msg.channel.send(`Mensagem será apagada às:  <t:${Math.floor(Date.now()/1000)+ purge_messages/1000}:R>`)
-				msg.channel.send("**"+matches['bestMatch']['target'].charAt(0).toUpperCase() + matches['bestMatch']['target'].slice(1).replace("_"," ")+"*");
-				msg.channel.send({files: [`${path}/${matches['bestMatch']['target']}_img.png`,`${path}/${matches['bestMatch']['target']}_vet.png`,`${path}/${matches['bestMatch']['target']}_doc.png`]});
-			}else{
-				delete_all_expect_pin()
-				msg.delete();
-				msg.channel.send(`Mensagem será apagada às:  <t:${Math.floor(Date.now()/1000)+ purge_messages/1000}:R>`)
-				msg.channel.send("**"+matches['bestMatch']['target'].charAt(0).toUpperCase() + matches['bestMatch']['target'].slice(1).replace("_"," ")+"*");
-				msg.channel.send({files: [`${path}/${matches['bestMatch']['target']}_img.png`,`${path}/${matches['bestMatch']['target']}_vet.png`]});
-			}
-			}else{
+			files_that_exist.push(file.replace("_vet.png", "").replace("_img.png", "").replace("_doc.png", ""))
+
+		}
+		while (unit.charAt(0) === '_') {
+			unit = unit.substring(1);
+		}
+		var matches = stringSimilarity.findBestMatch(unit, files_that_exist);
+		try {
+			if (fs.existsSync(`${path}/${matches['bestMatch']['target']}_img.png`) && fs.existsSync(`${path}/${matches['bestMatch']['target']}_vet.png`)) {
+				if (fs.existsSync(`${path}/${matches['bestMatch']['target']}_doc.png`)) {
+					delete_all_expect_pin()
+					msg.delete();
+					msg.channel.send(`Mensagem será apagada às:  <t:${Math.floor(Date.now()/1000)+ purge_messages/1000}:R>`)
+					msg.channel.send("**" + matches['bestMatch']['target'].charAt(0).toUpperCase() + matches['bestMatch']['target'].slice(1).replace("_", " ") + "*");
+					msg.channel.send({
+						files: [`${path}/${matches['bestMatch']['target']}_img.png`, `${path}/${matches['bestMatch']['target']}_vet.png`, `${path}/${matches['bestMatch']['target']}_doc.png`]
+					});
+				} else {
+					delete_all_expect_pin()
+					msg.delete();
+					msg.channel.send(`Mensagem será apagada às:  <t:${Math.floor(Date.now()/1000)+ purge_messages/1000}:R>`)
+					msg.channel.send("**" + matches['bestMatch']['target'].charAt(0).toUpperCase() + matches['bestMatch']['target'].slice(1).replace("_", " ") + "*");
+					msg.channel.send({
+						files: [`${path}/${matches['bestMatch']['target']}_img.png`, `${path}/${matches['bestMatch']['target']}_vet.png`]
+					});
+				}
+			} else {
 				msg.channel.send("Não existe uma unidade com esse nome");
 			}
-		  } catch(err) {
+		} catch (err) {
 			console.error(err)
-		  }
-	 }
-
-
-
-
-function delete_all_expect_pin()
-{
-    setTimeout(async function()
-    {   
-		const allMessages = await client.channels.cache.get(ID_channel_to_delete).messages.fetch()
-		const deletable = allMessages.filter(message => !message.pinned)
-
-		while(allMessages.filter(message => !message.pinned)){
-		const allMessages = await client.channels.cache.get(ID_channel_to_delete).messages.fetch()
-		const deletable = allMessages.filter(message => !message.pinned)
-		await client.channels.cache.get(ID_channel_to_delete).bulkDelete(deletable, true)
 		}
-    },purge_messages);
-}
+	}
+
+
+
+
+	function delete_all_expect_pin() {
+		setTimeout(async function() {
+			const allMessages = await client.channels.cache.get(ID_channel_to_delete).messages.fetch()
+			const deletable = allMessages.filter(message => !message.pinned)
+
+			while (allMessages.filter(message => !message.pinned)) {
+				const allMessages = await client.channels.cache.get(ID_channel_to_delete).messages.fetch()
+				const deletable = allMessages.filter(message => !message.pinned)
+				await client.channels.cache.get(ID_channel_to_delete).bulkDelete(deletable, true)
+			}
+		}, purge_messages);
+	}
 
 
 });
 
 async function get_google_sheets(nome) {
-    return new Promise((resolve, reject) => {
-    var currentDate = new Date();
-    var timestamp = currentDate.getTime();
-	axios.get("https://opensheet.elk.sh/1uUu5epwHjGf2ykQlHD4QfPhU-RS0FfYsgrFiMddXxlk/Presen%C3%A7as%20S%20XII?" + timestamp)
-	.then((response)=>{
-		//resolve(JSON.stringify(response.data))
-		response.data.forEach(element => {
-			
-            if(element['Nation']!='undefined' && element['Nation']!="")
-            if(element[" "].toLowerCase()==nome.toLowerCase()){
-            delete element['Nation'];
-            delete element['undefined'];
-            delete element['Current House'];
-            delete element[''];
-            delete element[' '];
-            var string = JSON.stringify(element, null, 2)
-            var tentative = (string.match(/\?/g) || []).length;
-            var presenca = (string.match(/✓/g) || []).length;
-            var falta = (string.match(/x/g) || []).length;
-            resolve("Presenças para :"+nome+"\nPresente: "+presenca+"\nFalta: "+falta+"\nTentative: " + tentative)
-            }
+	return new Promise((resolve, reject) => {
+		var currentDate = new Date();
+		var timestamp = currentDate.getTime();
+		axios.get("https://opensheet.elk.sh/1uUu5epwHjGf2ykQlHD4QfPhU-RS0FfYsgrFiMddXxlk/Presen%C3%A7as%20S%20XII?" + timestamp)
+			.then((response) => {
+				//resolve(JSON.stringify(response.data))
+				response.data.forEach(element => {
+
+					if (element['Nation'] != 'undefined' && element['Nation'] != "")
+						if (element[" "].toLowerCase() == nome.toLowerCase()) {
+							delete element['Nation'];
+							delete element['undefined'];
+							delete element['Current House'];
+							delete element[''];
+							delete element[' '];
+							var string = JSON.stringify(element, null, 2)
+							var tentative = (string.match(/\?/g) || []).length;
+							var presenca = (string.match(/✓/g) || []).length;
+							var falta = (string.match(/x/g) || []).length;
+							resolve("Presenças para :" + nome + "\nPresente: " + presenca + "\nFalta: " + falta + "\nTentative: " + tentative)
+						}
 
 
-        
-        
-        
-        });
-		
-	});
-})
+
+
+
+				});
+
+			});
+	})
 }
 
 client.login(process.env.TOKEN);
-
-
