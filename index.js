d7cb3c95a252fa465752108f86c6246d06125e54
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-12 20:15:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ client.on('message', async msg => {
 									//.setThumbnail(data[0])
 									//.setImage(data[2])
 
-					msg.channel.send({ embeds: [Embed, Embed1] });
+					msg.channel.send(Embed);
 
 			
 
