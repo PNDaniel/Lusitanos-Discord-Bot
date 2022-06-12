67f268d742917c5c74d83bea041482e4ff654826
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-12 20:44:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -126,10 +126,6 @@ client.on('message', async msg => {
 			unit = unit.substring(1);
 		}
 		var matches = stringSimilarity.findBestMatch(unit, files_that_exist);
-		msg.channel.send(matches['bestMatch']['target']);
-
-
-		
 		
 		try {
 					delete_all_expect_pin()
