2a7a06ab1d7ad5ef43de752a49390eec03eb807d
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-12 20:11:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -139,19 +139,19 @@ client.on('message', async msg => {
 
 					var data = await get_unit_linkV3(matches['bestMatch']['target']);
 
-					console.log(data[0],data[1],data[2])
+					
 
 					const Embed = new MessageEmbed()
 									.setColor('#0099ff')
 									.setTitle('Guide for '+"**" + matches['bestMatch']['target'].charAt(0).toUpperCase() + matches['bestMatch']['target'].slice(1).replace("_", " ") + "**")
 									.setDescription('Unit guide text can be implemented here!')
 									//.setThumbnail(data[0])
-									.setImage(data[1])
+									//.setImage(data[1])
 					const Embed1 = new MessageEmbed()
 									.setColor('#0099ff')
 									.setDescription('Veterancy')
 									//.setThumbnail(data[0])
-									.setImage(data[2])
+									//.setImage(data[2])
 
 					msg.channel.send({ embeds: [Embed, Embed1] });
 			
