2e9fecdc5fee3dc33c162abc09ca37e6a9108ef4
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-12 01:28:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const DiscordButtons = require('discord-buttons-v13'); //Requiring Discord-BUtto
 const ButtonPages = require('discord-button-pages'); //Requiring Discord-Button-Pages module.
 DiscordButtons(client);
 const path = './images'
-const purge_messages = 10000;
+const purge_messages = 10000; //milliseconds
 const ID_channel_to_delete = '984569178293940297';
 const ID_channel_request = '985278059001290753';
 
@@ -110,7 +110,7 @@ client.on('message', async msg => {
 			}else{
 				delete_all_expect_pin()
 				msg.delete();
-				msg.channel.send("Mensagem será apagada às:  <t:"+Math.floor(Date.now()/1000 + purge_messages/1000)+">")
+				msg.channel.send(`Mensagem será apagada às:  <t:${Math.floor(Date.now()/1000)+ purge_messages/1000}:R>`)
 				msg.channel.send(matches['bestMatch']['target'].charAt(0).toUpperCase() + matches['bestMatch']['target'].slice(1).replace("_"," "));
 				msg.channel.send({files: [`${path}/${matches['bestMatch']['target']}_img.png`,`${path}/${matches['bestMatch']['target']}_vet.png`]});
 			}
