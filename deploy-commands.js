e5101fcc4990436a6f86b4cff2d422bacf45ebad
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-13 00:27:58 +0100

    .

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId } = require('./config.json');
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
@@ -11,6 +12,6 @@ const commands = [
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands("984570826206621727", "984569178293940294"), { body: commands })
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
