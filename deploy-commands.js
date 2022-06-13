240b7219f14afa00e3daa4f759f1bf80a9b842ad
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-13 09:44:38 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,9 +4,16 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId } = require('./config.json');
 
 const commands = [
-	new SlashCommandBuilder().setName('unit').setDescription('Guide for specific unit with veterancy and doctrines        Usage:/unit=iron reapers'),
+	//new SlashCommandBuilder().setName('unit').setDescription('Guide for specific unit with veterancy and doctrines        Usage:/unit=iron reapers'),
 	new SlashCommandBuilder().setName('request').setDescription('Request to change the veterancy or doctrines of a unit.    Screenshot much be included.'),
-	new SlashCommandBuilder().setName('activity').setDescription('Get your presence in TW')
+	new SlashCommandBuilder().setName('activity').setDescription('Get your presence in TW'),
+	new SlashCommandBuilder()
+	.setName('unit')
+	.setDescription('Guide for specific unit with veterancy and doctrines        Usage:/unit=iron reapers')
+	.addStringOption(option =>
+		option.setName('Unit')
+			.setDescription('The unit you want to know information about')
+			.setRequired(true)),
 
 ]
 	.map(command => command.toJSON());
