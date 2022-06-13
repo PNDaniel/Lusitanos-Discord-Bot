9bf938e5e6799a873e47e138962acbee14fc502c
FabioAlmeida <36922235+fabio-r-almeida@users.noreply.github.com>
2022-06-13 01:00:57 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,6 +7,18 @@ const commands = [
 	new SlashCommandBuilder().setName('unit').setDescription('Guide for specific unit with veterancy and doctrines\nUso:/unit=iron reapers'),
 	new SlashCommandBuilder().setName('request').setDescription('Request to change the veterancy or doctrines of a unit. Screenshot much be included.'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+    new SlashCommandBuilder()
+	.setName('info')
+	.setDescription('Get info about a user or a server!')
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName('user')
+			.setDescription('Info about a user')
+			.addUserOption(option => option.setName('target').setDescription('The user')))
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName('server')
+			.setDescription('Info about the server')),
 ]
 	.map(command => command.toJSON());
  
